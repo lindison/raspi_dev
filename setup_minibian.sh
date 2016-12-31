ed90591e5f203f7780c5d55dc5c8243b50ebe77a
lindison <openstack@lindison.com>
2016-12-31 18:46:34 +0000

    updates

--- setup_minibian.sh
+++ setup_minibian.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# update Pi
 apt-get update
 apt-get upgrade -y
+
+# Install bash-completions
+apt-get install bash-completion
+
+#  Install git and raspi-config
 apt-get install git raspi-config -y
+
+# Install Python
+apt-get install python-dev
+
+
+# setup pip
+
+
