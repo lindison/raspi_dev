e2367fc99fad9050e6079def9848a5d028556e6a
Lindis <me@lindison.com>
2017-01-01 01:11:47 -0700

    updates

--- gpio/lab02/switch.py
+++ gpio/lab02/switch.py
@@ -1,3 +1,15 @@
-import RPi.GPIO as GPIO
-import time
+import RPi.GPIO as GPIO         ## Import GPIO Library
+
+inPin = 8                       ## Switch connected to Pin 8
+GPIO.setmode(GPIO.BOARD)        ## Use BOARD pin numbering
+GPIO.setup(inPin, GPIO.IN)      ## Set pin 8 to INPUT
+
+while True:                     ## Do this forever
+    value = GPIO.input(inPin)   ## Read input from switch
+    if value:                   ## If switch is released
+        print "Not Pressed"
+    else:                       ## Else print this
+        print "Pressed"
+
+GPIO.cleanup()
 
