acedd388073b15aec62cfd095e2884730eb14f5a
lindison <openstack@lindison.com>
2017-01-02 18:07:35 +0000

    updates

--- gpio/lab02/light_switch.py
+++ gpio/lab02/light_switch.py
@@ -1,15 +1,20 @@
 import RPi.GPIO as GPIO         ## Import GPIO Library
 
+pin = 11
 inPin = 7                       ## Switch connected to Pin 7
 GPIO.setmode(GPIO.BOARD)        ## Use BOARD pin numbering
-GPIO.setup(inPin, GPIO.IN)      ## Set pin 8 to INPUT
+GPIO.setup(inPin, GPIO.IN)      ## Set pin 7 to INPUT
+GPIO.setup(pin, GPIO.OUT)       ## Setup GPIO pin 7
 
 while True:                     ## Do this forever
     value = GPIO.input(inPin)   ## Read input from switch
     if value:                   ## If switch is released
-        print "Not Pressed"
+        print "Light On"
+        GPIO.output(pin, GPIO.LOW)
     else:                       ## Else print this
-        print "Pressed"
+        print "Light Off"
+        GPIO.output(pin, GPIO.HIGH)
 
 GPIO.cleanup()
+
 
